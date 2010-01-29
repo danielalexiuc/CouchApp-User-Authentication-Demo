aef5caf9ef3c7fdc7bad2914f10bd32d732b5fff
Daniel Alexiuc <daniel@alexiuc.com>
2010-01-29 15:09:12 +1000

    remove unneeded code

--- _attachments/javascripts/futon.js
+++ _attachments/javascripts/futon.js
@@ -1,15 +1,3 @@
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// use this file except in compliance with the License. You may obtain a copy of
-// the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations under
-// the License.
-
 (function($) {
 
   function Session() {
@@ -27,7 +15,7 @@
           callback({username : "Error logging in: "+reason});
         }
       });
-    };
+    }
 
     function doSignup(username, password, callback, runLogin) {
       $.couch.signup({
@@ -49,19 +37,19 @@
           }
         }
       });
-    };
+    }
 
     function validateUsernameAndPassword(data, callback) {
       if (!data.username || data.username.length == 0) {
         callback({username: "Please enter a username."});
         return false;
-      };
+      }
       if (!data.password || data.password.length == 0) {
         callback({password: "Please enter a password."});
         return false;
-      };
+      }
       return true;
-    };
+    }
 
     function createAdmin() {
       $.showDialog("dialog/create_admin.html", {
@@ -77,7 +65,7 @@
         }
       });
       return false;
-    };
+    }
 
     function login() {
       $.showDialog("dialog/login.html", {
@@ -87,7 +75,7 @@
         }
       });
       return false;
-    };
+    }
 
     function logout() {
       $.couch.logout({
@@ -95,7 +83,7 @@
           $.futon.session.sidebar();
         }
       })
-    };
+    }
 
     function signup() {
       $.showDialog("dialog/signup.html", {
@@ -105,7 +93,7 @@
         }
       });
       return false;
-    };
+    }
 
     this.setupSidebar = function() {
       $("#userCtx .login").click(login);
@@ -130,124 +118,9 @@
             $("#userCtx .adminparty").show();
           } else {
             $("#userCtx .loggedout").show();
-          };
+          }
         }
       })
-    };
-  };
-
-  function Navigation() {
-    var nav = this;
-    this.loaded = false;
-    this.eventHandlers = {
-      load: []
-    };
-
-    this.ready = function(callback) {
-      if (callback) {
-        if (this.loaded) {
-          callback.apply(this);
-        }
-        this.eventHandlers["load"].push(callback);
-      } else {
-        this.loaded = true;
-        callbacks = this.eventHandlers["load"];
-        for (var i = 0; i < callbacks.length; i++) {
-          callbacks[i].apply(this);
-        }
-      }
-    }
-
-    this.addDatabase = function(name) {
-      var recentDbs = $.futon.storage.get("recent", "").split(",");
-      if ($.inArray(name, recentDbs) == -1) {
-        recentDbs.unshift(name);
-        if (recentDbs.length > 10) recentDbs.length = 10;
-        $.futon.storage.set("recent", recentDbs.join(","));
-        this.updateDatabases();
-      }
-    }
-
-    this.removeDatabase = function(name) {
-      // remove database from recent databases list
-      var recentDbs = $.futon.storage.get("recent").split(",");
-      var recentIdx = $.inArray(name, recentDbs);
-      if (recentIdx >= 0) {
-        recentDbs.splice(recentIdx, 1);
-        $.futon.storage.set("recent", recentDbs.join(","));
-        this.updateDatabases();
-      }
-    }
-
-    this.updateDatabases = function() {
-      var selection = null;
-      $("#dbs .selected a").each(function() {
-        selection = [this.pathname, this.search];
-      });
-      $("#dbs").empty();
-      var recentDbs = $.futon.storage.get("recent").split(",");
-      recentDbs.sort();
-      $.each(recentDbs, function(idx, name) {
-        if (name) {
-          $("#dbs").append("<li>" +
-            "<button class='remove' title='Remove from list' value='" + name + "'></button>" +
-            "<a href='database.html?" + encodeURIComponent(name) + "' title='" + name + "'>" + name +
-            "</a></li>");
-        }
-      });
-      if (selection) {
-        this.updateSelection(selection[0], selection[1]);
-      }
-      $("#dbs button.remove").click(function() {
-        nav.removeDatabase(this.value);
-        return false;
-      });
-    }
-
-    this.updateSelection = function(path, queryString) {
-      function fixupPath(path) { // hack for IE/Win
-        return (path.charAt(0) != "/") ? ("/" + path) : path;
-      }
-      if (!path) {
-        path = location.pathname;
-        if (!queryString) {
-          queryString = location.search;
-        }
-      } else if (!queryString) {
-        queryString = "";
-      }
-      var href = fixupPath(path + queryString);
-      $("#nav li").removeClass("selected");
-      $("#nav li a").each(function() {
-        if (fixupPath(this.pathname) + this.search != href) return;
-        $(this).parent("li").addClass("selected").parents("li").addClass("selected");
-      });
-    }
-
-    this.toggle = function(speed) {
-      if (speed === undefined) {
-        speed = 500;
-      }
-      var sidebar = $("#sidebar").stop(true, true);
-      var hidden = !$(sidebar).is(".hidden");
-
-      $("#wrap").animate({
-        marginRight: hidden ? 0 : 210
-      }, speed, function() {
-        $(document.body).toggleClass("fullwidth", hidden);
-      });
-      sidebar.toggleClass("hidden").animate({
-        width: hidden ? 26 : 210,
-        height: hidden ? $("h1").outerHeight() - 1 : "100%",
-        right: hidden ? 0 : -210
-      }, speed).children(":not(#sidebar-toggle)").animate({
-        opacity: "toggle"
-      }, speed);
-      $("h1").animate({marginRight: hidden ? 26 : 0}, speed);
-
-      $("#sidebar-toggle")
-        .attr("title", hidden ? "Show Sidebar" : "Hide Sidebar");
-      $.futon.storage.set("sidebar", hidden ? "hidden" : "show");
     };
   }
 
@@ -349,14 +222,11 @@
           window.name = JSON.stringify(obj);
         }
       }
-
     };
-
   }
 
   $.futon = $.futon || {};
   $.extend($.futon, {
-    navigation: new Navigation(),
     session : new Session(),
     storage: new Storage()
   });
@@ -416,24 +286,10 @@
   $.futon.storage.declare("recent", {scope: "cookie", defaultValue: ""});
 
   $(function() {
-    document.title = "Apache CouchDB - Futon: " + document.title;
-    if ($.futon.storage.get("sidebar") == "hidden") {
-      // doing this as early as possible prevents flickering
-      $(document.body).addClass("fullwidth");
-    }
+    document.title = "CouchApp User Authentication Demo: " + document.title;
     $.get("sidebar.html", function(resp) {
-      $("#wrap").append(resp)
-        .find("#sidebar-toggle").click(function(e) {
-            $.futon.navigation.toggle(e.shiftKey ? 2500 : 500);
-            return false;
-          });
-      if ($.futon.storage.get("sidebar") == "hidden") {
-        $.futon.navigation.toggle(0);
-      }
+      $("#wrap").append(resp);
 
-      $.futon.navigation.updateDatabases();
-      $.futon.navigation.updateSelection();
-      $.futon.navigation.ready();
       $.futon.session.setupSidebar();
       $.futon.session.sidebar();
 
